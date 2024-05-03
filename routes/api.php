4e9050dc07ba9e9286a2b1be2641c8faf6347cfc
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-05-03 20:33:14 +0530

    basic views and pass data

--- routes/api.php
+++ routes/api.php
@@ -3,16 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/posts', function (){
-    return response()->json([
-        'posts' => [
-            [
-                'title' => 'Post One',
-            ]
-        ]
-    ]);
-});
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
