3f19d600080f013cfe761232d1cd4510ea52dbda
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-05-03 18:26:36 +0530

    create a basic route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return response('<h1>Hello World</h1>', 200)
+        ->header('Content-Type', 'text/plain')
+        ->header('foo', 'bar');
+});
