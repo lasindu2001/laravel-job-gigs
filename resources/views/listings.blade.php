ade2177acbc5452d40f4bf485ab679be5602c078
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-05-03 21:29:39 +0530

    create a basic model

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -2,28 +2,12 @@
     {{$heading}}
 </h1>
 
-@php
-   $test = 1; 
-@endphp
-{{$test}}
-
-{{-- @if(count($listings) == 0)
-    <p>No listings found</p>
-@endif
-
-@foreach($listings as $listing)
-    <h2>
-        {{$listing['title']}}
-    </h2>
-    <p>
-        {{$listing['description']}}
-    </p>
-@endforeach --}}
-
 @unless(count($listings) == 0)
     @foreach($listings as $listing)
     <h2>
-        {{$listing['title']}}
+        <a href="/listings/{{$listing['id']}}">
+            {{$listing['title']}}
+        </a>
     </h2>
     <p>
         {{$listing['description']}}
