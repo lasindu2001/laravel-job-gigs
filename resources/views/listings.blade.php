ae051597ca893875bdb75e37789ebecdec37b934
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-05-03 20:52:01 +0530

    blade templates and basic directives

---
+++ resources/views/listings.blade.php
@@ -0,0 +1,34 @@
+<h1>
+    {{$heading}}
+</h1>
+
+@php
+   $test = 1; 
+@endphp
+{{$test}}
+
+{{-- @if(count($listings) == 0)
+    <p>No listings found</p>
+@endif
+
+@foreach($listings as $listing)
+    <h2>
+        {{$listing['title']}}
+    </h2>
+    <p>
+        {{$listing['description']}}
+    </p>
+@endforeach --}}
+
+@unless(count($listings) == 0)
+    @foreach($listings as $listing)
+    <h2>
+        {{$listing['title']}}
+    </h2>
+    <p>
+        {{$listing['description']}}
+    </p>
+    @endforeach
+@else
+    <p>No listings found</p>
+@endunless
