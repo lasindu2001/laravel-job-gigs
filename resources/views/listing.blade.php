4881e18fc47b31df3273cb384b03c21293b041d2
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-05-04 16:32:57 +0530

    modify component attributes

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -6,7 +6,7 @@
     ><i class="fa-solid fa-arrow-left"></i> Back
 </a>
 <div class="mx-4">
-    <x-card>
+    <x-card class="bg-black">
         <div
             class="flex flex-col items-center justify-center text-center"
         >
