3eef758f9bab9895bb842ece9a352daa932c0b46
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-05-05 13:31:39 +0530

    create a listing form

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -7,15 +7,22 @@ use Illuminate\Http\Request;
 
 class ListingController extends Controller
 {
+    // Show All Listings
     public function index(){
         return view('listings.index', [
             'listings' => Listing::latest()->filter(request(['tag', 'search']))->get()
         ]);
     }
 
+    // Show Single Listing
     public function show(Listing $listing){
         return view('listings.show', [
             'listing' => $listing
         ]);
+    }
+
+    // Shoe Create Form
+    public function create(){
+        return view('listings.create');
     }
 }
