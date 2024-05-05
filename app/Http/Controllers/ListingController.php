e364cf6d8ff0657c30e350d9d0002b0f358abeb8
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-05-05 18:29:33 +0530

    add pagination

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -11,7 +11,7 @@ class ListingController extends Controller
     // Show All Listings
     public function index(){
         return view('listings.index', [
-            'listings' => Listing::latest()->filter(request(['tag', 'search']))->get()
+            'listings' => Listing::latest()->filter(request(['tag', 'search']))->paginate(6) // Also can use simplePaginate()
         ]);
     }
 
