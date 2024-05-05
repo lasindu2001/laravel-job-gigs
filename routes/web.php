442a536b9f09cb166fdaed4007f8af9fa1cd2c46
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-05-05 20:54:42 +0530

    complete update listing functionality

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,11 @@ Route::get('/listings/create', [ListingController::class, 'create']);
 // Store Listing Data
 Route::post('/listings', [ListingController::class, 'store']);
 
+// Show Edit Form
+Route::get('/listings/{listing}/edit', [ListingController::class, 'edit']);
+
+// Update Listing Data
+Route::put('/listings/{listing}', [ListingController::class, 'update']);
+
 // Single Listing
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
