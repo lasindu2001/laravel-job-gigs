9715aa0d54297f3e0257fd1c2637d7bc52d8b5a6
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-05-06 15:38:07 +0530

    add user authorization

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -54,12 +54,22 @@ class ListingController extends Controller
 
     // Show Edit Form
     public function edit(Listing $listing){
+
+        // Make sure logged in user is owner
+        if($listing->user_id != auth()->id()){
+            abort(403, 'Unauthorized Action');
+        }
         
         return view('listings.edit', ['listing' => $listing]);
     }
 
     // Update Listing Data
     public function update(Request $request, Listing $listing){
+
+        // Make sure logged in user is owner
+        if($listing->user_id != auth()->id()){
+            abort(403, 'Unauthorized Action');
+        }
         
         $formFields = $request->validate([
             'title' => 'required',
@@ -83,8 +93,16 @@ class ListingController extends Controller
 
     // Delete Listing
     public function destroy(Listing $listing){
+
+        // Make sure logged in user is owner
+        if($listing->user_id != auth()->id()){
+            abort(403, 'Unauthorized Action');
+        }
+
         $listing->delete();
+
         return redirect('/')->with('message', 'Listing Deleted Successfully');
+
     }
 
     // Manage Listings
