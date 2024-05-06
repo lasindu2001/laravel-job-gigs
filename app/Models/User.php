55bb12701ffdedbf6bfb9549b585c8e1cf21711d
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-05-06 13:46:19 +0530

    add relationships

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,4 +44,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    // Relationship With Listings
+    public function listings(){
+        return $this->hasMany(Listing::class, 'user_id');
+    }
 }
--- database/migrations/2024_05_03_161532_create_listings_table.php
+++ database/migrations/2024_05_03_161532_create_listings_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('listings', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('title');
             $table->string('logo')->nullable();
             $table->string('tags');
@@ -20,7 +21,7 @@ return new class extends Migration
             $table->string('location');
             $table->string('email');
             $table->string('website');
-            $table->string('description');
+            $table->text('description');
             $table->timestamps();
         });
     }
